8cc779df66ed379b77152c6f0bc705a996557347
lucahsieh <chungju.hsieh.ca@gmail.com>
2022-04-02 10:35:38 -0700

    completed

---
+++ src/views/RegistrationView.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="home flex-v-center full-page">
+    <cli-progressbar></cli-progressbar>
+  </div>
+</template>
+
+<script>
+import CliProgressBar from "../components/CliProgressBar.vue";
+
+export default {
+  name: "HomeView",
+  components: { "cli-progressbar": CliProgressBar },
+  data() {
+    return {};
+  },
+};
+</script>
+
+<style scoped></style>
